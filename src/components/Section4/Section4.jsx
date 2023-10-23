ad1da94d9969a19b627175d4e42e081b72282846
mohamed <mohamayman770@gmail,com>
2023-10-23 03:45:54 +0300

    file:fix

--- src/components/Section4/Section4.jsx
+++ src/components/Section4/Section4.jsx
@@ -4,8 +4,8 @@ import sunglass from '../../assets/Images/sunglasses.png'
 import styled from './Section4.module.css'
 
 const Section4 = () => {
-  return <section name='section4' id='sec4' className='py-3 '>
-    <div className="container mt-3">
+  return <section name='section4' id='sec4' className='py-4 '>
+    <div className="container mt-5">
         <div className="row align-items-center ">
         <div  className="col-md-6">
             <div style={{width:'250px'}} className="mx-1 img">
